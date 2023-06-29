4f68350db7f8f5c8a3df53dfde4004e40a767410
Cyril-Crinquette <twousy734@gmail.com>
2023-06-29 10:07:27 +0200

    Page OneNews finalisée

--- src/screen/LastProductsHome.js
+++ src/screen/LastProductsHome.js
@@ -1,12 +1,82 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useRoute } from '@react-navigation/native'
+import { instanceAxios } from '../utils/interceptor'
+import { Image } from 'native-base'
 
 const LastProductsHome = () => {
+  const route = useRoute()
+  const { id } = route.params
+  const [product, setProduct] = useState()
+
+  useEffect(() => {
+    instanceAxios
+      .get(`product/${id}`)
+      .then((res) => {
+        setProduct(res.data)
+      })
+      .catch((error) => {
+        setProduct([])
+      })
+  }, [])
   return (
-    <View>
-      <Text>LastProductsHome</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollViewContent}>
+      <View style={styles.container}>
+        <Text style={styles.productName}>{product?.name}</Text>
+        <Image
+          source={{
+            uri: `https://api-gyozilla.onrender.com/${product?.image}`,
+          }}
+          style={styles.productImage}
+          resizeMode="cover"
+          alt="Image du produit"
+        />
+        <Text style={styles.productDescription}>{product?.description}</Text>
+      </View>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingVertical: 24,
+    backgroundColor: '#FFFFFF',
+  },
+  productImage: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+    marginBottom: 16,
+  },
+  productName: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  productPrice: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: '#FF0000',
+  },
+  productDescription: {
+    fontSize: 16,
+    marginBottom: 16,
+  },
+  addButton: {
+    backgroundColor: '#FF0000',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    borderRadius: 4,
+  },
+  addButtonText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+})
 
 export default LastProductsHome
--- src/screen/OneNews.js
+++ src/screen/OneNews.js
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from 'react'
 import { useRoute } from '@react-navigation/native'
 import { instanceAxios } from '../utils/interceptor'
 import { Image } from 'native-base'
+import Loader from '../components/loader'
 
 const OneNews = () => {
   const route = useRoute()
   const { id } = route.params
-  const [news, setNews] = useState()
+  const [news, setNews] = useState(null)
 
   useEffect(() => {
     instanceAxios
@@ -16,21 +17,25 @@ const OneNews = () => {
         setNews(res.data)
       })
       .catch((error) => {
-        setNews([])
+        setNews(null)
       })
   }, [])
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContent}>
-      <View style={styles.container}>
-        <Text style={styles.newsName}>{news?.name}</Text>
-        <Image
-          source={{ uri: `https://api-gyozilla.onrender.com/${news?.image}` }}
-          style={styles.newsImage}
-          resizeMode="cover"
-          alt="Image de l'actualité"
-        />
-        <Text style={styles.newsDescription}>{news?.description}</Text>
-      </View>
+      {news ? (
+        <View style={styles.container}>
+          <Text style={styles.newsName}>{news?.name}</Text>
+          <Image
+            source={{ uri: `https://api-gyozilla.onrender.com/${news?.image}` }}
+            style={styles.newsImage}
+            resizeMode="cover"
+            alt="Image de l'actualité"
+          />
+          <Text style={styles.newsDescription}>{news?.description}</Text>
+        </View>
+      ) : (
+        <Loader />
+      )}
     </ScrollView>
   )
 }
@@ -55,11 +60,13 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     backgroundColor: '#faeccb',
     padding: 10,
+    borderRadius: 8,
   },
   newsImage: {
-    width: '100%',
-    height: 250,
+    width: 450,
+    height: 350,
     marginTop: 10,
+    borderRadius: 4,
   },
   newsDescription: {
     fontSize: 16,
